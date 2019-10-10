ac9c4a1946246182a0a3a7108a813d82dacd1f77
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-10-10 13:16:46 +0200

    Style changes and ListView Listeners

--- app/src/main/java/es/aizaguirre/notasapp/MainActivity.kt
+++ app/src/main/java/es/aizaguirre/notasapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var editTextTitle : EditText
     lateinit var editTextBody : EditText
     lateinit var listViewNotes : ListView
+    lateinit var adapter : ArrayAdapter<Note>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,36 +26,77 @@ class MainActivity : AppCompatActivity() {
         /**
          * We have to get the elements we need searching by id
          */
-        editTextTitle = findViewById(R.id.title)
-        editTextBody = findViewById(R.id.body)
-        listViewNotes = findViewById(R.id.noteList)
+        getLayoutViews()
 
         /**
          * In order to show the list of notes that has been saved
-         * we need an adaptor
+         * we need an adaptor that has been declared for the whole class
          */
-        val adapter = ArrayAdapter<Note>(this, android.R.layout.simple_list_item_1,
-            notes)
+        setAdapter()
+
+        setListViewListeners()
+
+    }
+
+    private fun setListViewListeners() {
+        listViewNotes.setOnItemClickListener { parent, view, position, id ->
+            val note = notes[position]
+            editTextTitle.setText(note.title)
+            editTextBody.setText(note.text)
+        }
+
+        listViewNotes.setOnItemLongClickListener { parent, view, position, id ->
+            notes.removeAt(position)
+            adapter.notifyDataSetChanged()
+            true
+        }
+    }
+
+    private fun setAdapter() {
+        adapter = ArrayAdapter<Note>(
+            this, android.R.layout.simple_list_item_1,
+            notes
+        )
 
 
         /**
          * We set the 'adapter' property of our listView object to the adapter we have created
          */
         listViewNotes.adapter = adapter
+    }
 
+    private fun getLayoutViews() {
+        editTextTitle = findViewById(R.id.title)
+        editTextBody = findViewById(R.id.body)
+        listViewNotes = findViewById(R.id.noteList)
     }
 
     fun addNote(v : View?){
 
-        /**
-         * We have to get the title, the text, create a Note and add to the list
-         */
-        val title = editTextTitle.text.toString()
-        val body = editTextBody.text.toString()
-        val date = Date()
+        if(editTextTitle.text.toString() != "") {
 
-        val note = Note(title, body, date)
+            /**
+             * We have to get the title, the text, create a Note and add to the list
+             */
+            val title = editTextTitle.text.toString()
+            val body = editTextBody.text.toString()
+            val date = Date()
 
-        notes.add(note)
+            val note = Note(title, body, date)
+
+            notes.add(note)
+            adapter.notifyDataSetChanged()
+            resetNoteViews()
+        } else {
+
+            var msg = resources.getString(R.string.emptyTitle)
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun resetNoteViews(){
+        editTextBody.setText("")
+        editTextTitle.setText("")
     }
 }
--- app/src/main/java/es/aizaguirre/notasapp/Note.kt
+++ app/src/main/java/es/aizaguirre/notasapp/Note.kt
@@ -2,4 +2,10 @@ package es.aizaguirre.notasapp
 
 import java.util.*
 
-data class Note (var title : String, var text: String, var date : Date)
+data class Note (var title : String, var text: String, var date : Date){
+
+    /**
+     * Since it's only a simple expression we don't need to use the whole function structure
+     */
+    override fun toString(): String = title
+}
